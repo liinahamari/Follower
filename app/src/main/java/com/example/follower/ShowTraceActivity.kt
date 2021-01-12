bef3b481f148de5382f0d74f096a5e28a7464821
liinahamari <l1bills@protonmail.com>
2021-01-12 22:40:31 +0200

    Showing actual data in ShowTraceActivity

--- app/src/main/java/com/example/follower/ShowTraceActivity.kt
+++ app/src/main/java/com/example/follower/ShowTraceActivity.kt
@@ -1,13 +1,57 @@
 package com.example.follower
 
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.activity_show_trace.*
+import java.util.*
+import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.schedulers.Schedulers
 
 class ShowTraceActivity : AppCompatActivity() {
+    private val disposable = CompositeDisposable()
+    private val mapper = AddressMapper()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_trace)
-        logsContainer.text = FlightRecorder.getEntireRecord()
+
+        disposable += Single.fromCallable {
+            FlightRecorder.getEntireRecord()
+                .split("\n\n")
+                .filter { it.contains("Location Changed") }
+                .joinToString(separator = "\n") {
+                    val (lat, long) = "Location Changed\\. lat:(\\d+\\.\\d+), long:(\\d+\\.\\d+)".toRegex().find(it)!!.groupValues.drop(1)
+                    mapper.transform(lat.toDouble(), long.toDouble())
+                }
+        }
+            .doOnError { FlightRecorder.e(stackTrace = it.stackTrace) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(Consumer {
+                logsContainer.text = it
+            })
+    }
+}
+
+class AddressMapper {
+    fun transform(lat: Double, long: Double): String {
+        /*
+        val addresses: List<Address> = Geocoder(App.INSTANCE, Locale.UK).getFromLocation(lat, long, 1)
+
+        // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+        val address: String = addresses[0].getAddressLine(0)
+        val city: String = addresses[0].locality
+        val state: String = addresses[0].adminArea
+        val country: String = addresses[0].countryName
+        val postalCode: String? = addresses[0].postalCode
+        val knownName: String = addresses[0].featureName
+        return "Address: $address; City: $city; State: $state; Country: $country; Postal code: $postalCode; knownName: $knownName\n"
+        */
+        return Geocoder(App.INSTANCE, Locale.UK).getFromLocation(lat, long, 1).first().getAddressLine(0)
     }
 }
