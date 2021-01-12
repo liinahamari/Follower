68d51cab130d238acfaec0781f5b4bcc0efc7b01
liinahamari <l1bills@protonmail.com>
2021-01-13 00:02:08 +0200

    Registering notification channel in Application subclass

--- app/src/main/java/com/example/follower/App.kt
+++ app/src/main/java/com/example/follower/App.kt
@@ -1,11 +1,14 @@
 package com.example.follower
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
+        getSystemService(NotificationManager::class.java).createNotificationChannel(NotificationChannel(CHANNEL_ID, "GPS tracker", NotificationManager.IMPORTANCE_DEFAULT))
     }
 
     companion object {
