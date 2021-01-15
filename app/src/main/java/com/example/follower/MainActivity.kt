f0ab5efcc8ba51804a2b3bbb9ccd0efef7f1319d
liinahamari <l1bills@protonmail.com>
2021-01-16 00:11:27 +0200

    Bugfix for changing bottom navigation bar index on activity recreation

--- app/src/main/java/com/example/follower/MainActivity.kt
+++ app/src/main/java/com/example/follower/MainActivity.kt
@@ -10,21 +10,30 @@ import com.example.follower.screens.map.MapFragment
 import com.example.follower.screens.tracking_control.TrackingControlFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val PAGE_EXTRA = "MainActivity.page_extra"
+
 class MainActivity : BaseActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        pager.adapter = MainScreenViewPagerAdapter(supportFragmentManager)
-        globalMenu.setupWithViewPager(pager)
+        with(pager) {
+            adapter = MainScreenViewPagerAdapter(supportFragmentManager)
+            globalMenu.setupWithViewPager(this)
+        }
+    }/*todo bug with different states of menu and pager*/
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        globalMenu.selectTabAt(savedInstanceState.getInt(PAGE_EXTRA), false)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(PAGE_EXTRA, pager.currentItem)
     }
 
     private inner class MainScreenViewPagerAdapter(supportFragmentManager: FragmentManager) : FragmentStatePagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-        override fun getCount(): Int = 3
-
-        override fun getItem(position: Int): Fragment = when (position) {
-            0 -> TrackingControlFragment()
-            1 -> MapFragment()
-            2 -> SettingsFragment()
-            else -> throw IllegalStateException()
-        }
+        private val fragments = arrayOf(TrackingControlFragment(), MapFragment(), SettingsFragment())
+        override fun getCount(): Int = fragments.size
+        override fun getItem(position: Int): Fragment = fragments[position]
     }
 }
--- app/src/main/java/com/example/follower/model/Trace.kt
+++ app/src/main/java/com/example/follower/model/Trace.kt
@@ -1,4 +1,5 @@
 package com.example.follower.model
 
-class Trace {
-}
+data class Trace(val startTime: Long, val startTimeReadable: String, val points: List<TracePoint>)
+
+data class TracePoint(val longitude: Double, val latitude: Double)
