abb179cad4ba08d374d650ef78912d7a6cda8579
liinahamari <l1bills@protonmail.com>
2021-01-17 20:02:10 +0200

    shake detecting in MainActivity

--- app/src/main/java/com/example/follower/MainActivity.kt
+++ app/src/main/java/com/example/follower/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.follower
 
+import android.content.Intent
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -7,13 +9,24 @@ import androidx.fragment.app.FragmentStatePagerAdapter
 import com.example.follower.base.BaseActivity
 import com.example.follower.screens.SettingsFragment
 import com.example.follower.screens.map.MapFragment
+import com.example.follower.screens.show_trace.ShowTraceActivity
 import com.example.follower.screens.tracking_control.TrackingControlFragment
+import com.squareup.seismic.ShakeDetector
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 private const val PAGE_EXTRA = "MainActivity.page_extra"
 
-class MainActivity : BaseActivity(R.layout.activity_main) {
+class MainActivity : BaseActivity(R.layout.activity_main), ShakeDetector.Listener {
+    @Inject lateinit var sensorManager: SensorManager
+    private val shakeDetector: ShakeDetector by lazy { ShakeDetector(this) }
+    override fun hearShake() = startActivity(Intent(this, LogsActivity::class.java))
+
+    override fun onResume() = super.onResume().also { shakeDetector.start(sensorManager) }
+    override fun onPause() = super.onPause().also { shakeDetector.stop() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        (application as FollowerApp).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         with(pager) {
             adapter = MainScreenViewPagerAdapter(supportFragmentManager)
