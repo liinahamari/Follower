50c57e969486d69c560e526e00c5a020b4765bcd
liinahamari <l1bills@protonmail.com>
2021-01-26 02:28:32 +0300

    Replacing startActivityForResult with brand new registerForActivityResul

--- app/src/main/java/com/example/follower/screens/logs/LogsActivity.kt
+++ app/src/main/java/com/example/follower/screens/logs/LogsActivity.kt
@@ -1,10 +1,12 @@
 package com.example.follower.screens.logs
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.net.toFile
 import androidx.core.net.toUri
@@ -21,13 +23,13 @@ import kotlinx.android.synthetic.main.activity_logs.*
 import java.io.File
 import javax.inject.Inject
 
-private const val CREATE_DOC_REQUEST_CODE = 100500
 private const val MY_EMAIL = "l1bills@protonmail.com"
 private const val MESSAGE_TITLE = "Follower Logs"
 private const val TEXT_TYPE = "text/plain"
 
 class LogsActivity : BaseActivity(R.layout.activity_logs) {
     private val viewModel by viewModels<LogsActivityViewModel> { viewModelFactory }
+
     @Inject lateinit var logger: FlightRecorder
     @Inject lateinit var logFile: File
 
@@ -47,8 +49,6 @@ class LogsActivity : BaseActivity(R.layout.activity_logs) {
                 putExtra(Intent.EXTRA_EMAIL, arrayOf(MY_EMAIL))
                 putExtra(Intent.EXTRA_SUBJECT, MESSAGE_TITLE)
                 putExtra(Intent.EXTRA_STREAM, it)
-
-                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) /*todo remove?*/
             }.also { intent ->
                 startActivity(Intent.createChooser(intent, getString(R.string.title_pick_email_provider)))
             }
@@ -67,30 +67,12 @@ class LogsActivity : BaseActivity(R.layout.activity_logs) {
             .subscribe { requestFileCreation() }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when (requestCode) {
-            CREATE_DOC_REQUEST_CODE -> {
-                if (resultCode == Activity.RESULT_OK && data != null) {
-                    val sourceUri: Uri = logFile.toUri()
-                    val targetUri: Uri? = data.data
-                    targetUri?.let {
-                        viewModel.copyFile(sourceUri, targetUri)
-                    }
-                }
-            }
-            else -> super.onActivityResult(requestCode, resultCode, data)
+    private fun requestFileCreation() = registerForActivityResult(object : ActivityResultContracts.CreateDocument() {
+        override fun createIntent(context: Context, input: String): Intent = super.createIntent(context, input).apply {
+            type = TEXT_TYPE
+            addCategory(Intent.CATEGORY_OPENABLE)
         }
-    }
-
-    private fun requestFileCreation() {
-        try { /*TODO: replace with registerForActivityResult()*/
-            startActivityForResult(Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
-                addCategory(Intent.CATEGORY_OPENABLE)
-                type = TEXT_TYPE
-                putExtra(Intent.EXTRA_TITLE, DEFAULT_DEBUG_LOG_FILE_NAME)
-            }, CREATE_DOC_REQUEST_CODE)
-        } catch (e: Exception) {
-            errorToast(getString(R.string.title_file_manager_problem))
-        }
-    }
+    }) {
+        it?.let { viewModel.copyFile(logFile.toUri(), it) }
+    }.launch(DEFAULT_DEBUG_LOG_FILE_NAME)
 }
