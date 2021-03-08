ff0c213ccedd9ea17df69aa3ee26d0b17041a199
liinahamari <l1bills@protonmail.com>
2021-03-08 05:38:55 +0300

    Adding ErrorMessageFactory

--- app/src/main/java/com/example/follower/helper/ErrorMessageFactory.kt
+++ app/src/main/java/com/example/follower/helper/ErrorMessageFactory.kt
@@ -1,4 +1,16 @@
 package com.example.follower.helper
 
-class ErrorMessageFactory {
+import android.content.Context
+import com.example.follower.R
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ErrorMessageFactory @Inject constructor(private val context: Context) {
+    fun errorMessage(t: Throwable): String = when (t) {
+        is SocketTimeoutException, is UnknownHostException -> context.resources.getString(R.string.error_no_connection)
+        else -> context.resources.getString(R.string.error_unexpected)
+    }
 }
