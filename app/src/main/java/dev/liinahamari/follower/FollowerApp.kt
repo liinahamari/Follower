56233ffdb74ceca6e7b23e5c413a21788bc02790
liinahamari <l1bills@protonmail.com>
2021-04-16 08:59:07 +0300

    Adding license to every file

--- app/src/main/java/dev/liinahamari/follower/FollowerApp.kt
+++ app/src/main/java/dev/liinahamari/follower/FollowerApp.kt
@@ -1,3 +1,19 @@
+/*
+Copyright 2020-2021 liinahamari
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 @file:Suppress("NonConstantResourceId")
 
 package dev.liinahamari.follower
@@ -70,16 +86,17 @@ class FollowerApp : Application() {
         setupCrashStackTraceScreen()
     }
 
-    /*TODO: in some release version should be excluded or be optional*/
     private fun setupCrashStackTraceScreen() = Thread.setDefaultUncaughtExceptionHandler { thread, error ->
         logger.e("App Crash catch", error, false)
 
-        try {
-            startActivity(CrashStackTraceActivity.newIntent(this, thread.name, error))
-        } catch (e: Exception) {
-            logger.e("UncaughtExceptionHandler", e)
-        } finally {
-            exitProcess(1)
+        if (BuildConfig.DEBUG) {
+            try {
+                startActivity(CrashStackTraceActivity.newIntent(this, thread.name, error))
+            } catch (e: Exception) {
+                logger.e("UncaughtExceptionHandler", e)
+            } finally {
+                exitProcess(1)
+            }
         }
     }
 
--- app/src/main/java/dev/liinahamari/follower/screens/settings/AutoTrackingSchedulingUseCase.kt
+++ app/src/main/java/dev/liinahamari/follower/screens/settings/AutoTrackingSchedulingUseCase.kt
@@ -1,3 +1,19 @@
+/*
+Copyright 2020-2021 liinahamari
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 package dev.liinahamari.follower.screens.settings
 
 import android.app.AlarmManager
@@ -37,7 +53,7 @@ class AutoTrackingSchedulingUseCase constructor(
                 context.applicationContext,
                 AutoTrackingReceiver.ActionMode.ACTION_MODE_START.ordinal,
                 AutoTrackingReceiver.createIntent(AutoTrackingReceiver.ActionMode.ACTION_MODE_START, context.applicationContext),
-                FLAG_NO_CREATE
+                0
             )
         )
         alarmManager.cancel(
@@ -45,11 +61,13 @@ class AutoTrackingSchedulingUseCase constructor(
                 context.applicationContext,
                 AutoTrackingReceiver.ActionMode.ACTION_MODE_STOP.ordinal,
                 AutoTrackingReceiver.createIntent(AutoTrackingReceiver.ActionMode.ACTION_MODE_STOP, context.applicationContext),
-                FLAG_NO_CREATE
+                0
             )
         )
     }.toSingleDefault<CancelAutoTrackingResult>(CancelAutoTrackingResult.Success)
-        .onErrorReturn { CancelAutoTrackingResult.Failure }
+        .onErrorReturn {
+            it.printStackTrace()
+            CancelAutoTrackingResult.Failure }
         .doOnError { logger.e("Canceling auto-tracking", it) }
 
     fun setupStartAndStop(): Single<SchedulingStartStopResult> =
