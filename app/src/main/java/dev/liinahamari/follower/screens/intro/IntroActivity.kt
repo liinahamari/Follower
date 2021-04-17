5f20ea46bc2a9f3381a59176d44602b6e034bd65
liinahamari <l1bills@protonmail.com>
2021-04-17 20:23:59 +0300

    IntroActivity: Relying on app counter instead of pref_is_first_launch

--- app/src/main/java/dev/liinahamari/follower/screens/intro/IntroActivity.kt
+++ app/src/main/java/dev/liinahamari/follower/screens/intro/IntroActivity.kt
@@ -12,7 +12,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
 ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+*/
 
 package dev.liinahamari.follower.screens.intro
 
@@ -22,22 +22,23 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.github.appintro.AppIntro
 import dev.liinahamari.follower.R
-import dev.liinahamari.follower.ext.getBooleanOf
 import dev.liinahamari.follower.ext.getDefaultSharedPreferences
+import dev.liinahamari.follower.ext.getIntOf
 import dev.liinahamari.follower.ext.provideUpdatedContextWithNewLocale
 import dev.liinahamari.follower.screens.MainActivity
 
 class IntroActivity : AppIntro() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (getDefaultSharedPreferences().getBooleanOf(getString(R.string.pref_is_first_launch)).not()) {
+        if (getDefaultSharedPreferences().getIntOf(getString(R.string.pref_app_launch_counter)) != 1) {
             finish()
             startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            addSlide(SlideFragment.newInstance("Welcome to Follower!", "Application which tracks your movement even in background", R.color.purple_200, R.drawable.sc_background))
+            addSlide(SlideFragment.newInstance("Track observing", "Observe your track with Map or with Address View", R.color.purple_500, R.drawable.sc_address_list))
+            addSlide(SlideFragment.newInstance("Try Dark Theme", "Dark theme implemented to protect your eyes at night time", R.color.purple_700, R.drawable.sc_dark_map))
+            addSlide(SlideFragment.newInstance("Sharing and Import", "Share your tracks with your friends and observe their tracks!", R.color.purple_200, R.drawable.sc_share))
         }
-        addSlide(SlideFragment.newInstance("Welcome to Follower!", "Application which tracks your movement even in background", R.color.purple_200, R.drawable.sc_background))
-        addSlide(SlideFragment.newInstance("Track observing", "Observe your track with Map or with Address View", R.color.purple_500, R.drawable.sc_address_list))
-        addSlide(SlideFragment.newInstance("Try Dark Theme", "Dark theme implemented to protect your eyes at night time", R.color.purple_700, R.drawable.sc_dark_map))
-        addSlide(SlideFragment.newInstance("Sharing and Import", "Share your tracks with your friends and observe their tracks!", R.color.purple_200, R.drawable.sc_share))
     }
 
     override fun onSkipPressed(currentFragment: Fragment?) = startActivity(Intent(this, MainActivity::class.java))
