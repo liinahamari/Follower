183f0f9f8ad2629bc5792391a5693a4622a87f7f
liinahamari <me@liinahamari.dev>
2022-04-01 10:36:38 +0300

    Code cleanup in MainActivity of fakegps module

--- fakegps/src/main/java/dev/liinahamari/fakegps/MainActivity.kt
+++ fakegps/src/main/java/dev/liinahamari/fakegps/MainActivity.kt
@@ -14,65 +14,85 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.jakewharton.rxbinding4.view.clicks
+import dev.liinahamari.fakegps.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.plusAssign
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
 private const val FAKE_GEOLOCATION_INTERVAL = 3L
 private const val PERMISSION_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
-@RequiresApi(Build.VERSION_CODES.Q) const val PERMISSION_BACKGROUND_LOCATION = Manifest.permission.ACCESS_BACKGROUND_LOCATION
+
+@RequiresApi(Build.VERSION_CODES.Q)
+const val PERMISSION_BACKGROUND_LOCATION = Manifest.permission.ACCESS_BACKGROUND_LOCATION
+
+private const val LATITUDE_TALLINN = 59.436962
+private const val LONGITUDE_TALLINN = 24.753574
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    private val geoPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+    private val ui by viewBinding(ActivityMainBinding::bind)
+
+    private val locationPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
         if (it.values.all { accepted -> accepted }) {
             startMockingLocation()
         } else {
-            throw java.lang.RuntimeException()
+            throw RuntimeException()
         }
     }
 
-    private var lat = 59.43767452560608
-//    private var lat = 55.75578
-    private var lon = 24.754188013895778
-//    private var lon = 37.61786
+    private var actualLongitude = LONGITUDE_TALLINN
+    private var actualLatitude = LATITUDE_TALLINN
 
     private val clicks = CompositeDisposable()
     private val mockDisposable = CompositeDisposable()
-    private lateinit var locationManager: LocationManager
+    private val locationManager: LocationManager by lazy { getSystemService(Context.LOCATION_SERVICE) as LocationManager }
 
+    @SuppressLint("MissingSuperCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         setupClicks()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         clicks.clear()
+        mockDisposable.clear()
     }
 
     private fun setupClicks() {
-        clicks += btnAddGPS.clicks()
+        clicks += ui.btnAddGPS.clicks()
             .throttleFirst()
-            .subscribe({
-                addGPS()
-                btnAddGPS.isEnabled = false
-            }, { it.printStackTrace() })
-        clicks += btnDelGPS.clicks()
+            .subscribe {
+                addTestProvider()
+                ui.btnAddGPS.isEnabled = false
+            }
+        clicks += ui.btnDelGPS.clicks()
             .throttleFirst()
-            .subscribe({
-                delGPS()
-                btnAddGPS.isEnabled = true
-            }, { it.printStackTrace() })
+            .subscribe {
+                removeTestProvider()
+                ui.btnAddGPS.isEnabled = true
+            }
     }
 
-    private fun addGPS() {
-        locationManager.addTestProvider(LocationManager.GPS_PROVIDER, false, false, false, false, true, true, true, Criteria.POWER_LOW, Criteria.ACCURACY_FINE)
-        locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true)
+    private fun addTestProvider() {
+        locationManager.apply {
+            addTestProvider(
+                LocationManager.GPS_PROVIDER,
+                false,
+                false,
+                false,
+                false,
+                true,
+                true,
+                true,
+                Criteria.POWER_LOW,
+                Criteria.ACCURACY_FINE
+            )
+            setTestProviderEnabled(LocationManager.GPS_PROVIDER, true)
+        }
 
         val permissions = mutableListOf(PERMISSION_LOCATION)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -81,38 +101,37 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         if (hasAllPermissions(permissions)) {
             startMockingLocation()
         } else {
-            geoPermission.launch(permissions.toTypedArray())
+            locationPermission.launch(permissions.toTypedArray())
         }
     }
 
-    private fun delGPS() = locationManager.removeTestProvider(LocationManager.GPS_PROVIDER)
+    private fun removeTestProvider() = locationManager.removeTestProvider(LocationManager.GPS_PROVIDER)
         .also {
             mockDisposable.clear()
-        }.also {
-            lat = 55.75578
-            lon = 37.61786
+            actualLatitude = LATITUDE_TALLINN
+            actualLongitude = LONGITUDE_TALLINN
         }
 
     @SuppressLint("MissingPermission")
     private fun startMockingLocation() {
-        mockDisposable += Observable.interval(FAKE_GEOLOCATION_INTERVAL, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+        mockDisposable += Observable.interval(FAKE_GEOLOCATION_INTERVAL, TimeUnit.SECONDS)
             .map {
                 Location(LocationManager.GPS_PROVIDER)
                     .apply {
-                        lat -= 0.0005
-                        lon -= 0.0005
-                        latitude = lat
-                        longitude = lon
+                        actualLatitude -= 0.0005
+                        actualLongitude -= 0.0005
+                        latitude = actualLatitude
+                        longitude = actualLongitude
                         time = System.currentTimeMillis()
                         accuracy = 25f
                         elapsedRealtimeNanos = System.nanoTime()
                     }
             }
-            .subscribe { newLocation ->
-                locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation)
-            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(locationManager::setGpsTestProviderLocation)
     }
 }
 
+private fun LocationManager.setGpsTestProviderLocation(location: Location) = setTestProviderLocation(LocationManager.GPS_PROVIDER, location)
 fun Observable<Unit>.throttleFirst(skipDurationMillis: Long = 500L): Observable<Unit> = compose { it.throttleFirst(skipDurationMillis, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread()) }
 fun Activity.hasAllPermissions(permissions: List<String>): Boolean = permissions.all { ActivityCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED }
